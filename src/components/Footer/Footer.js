fa2b4781dc5ba5af55ed6d88dcbe24babab8d30f
Shrikant Yadav <shrikant9907@gmail.com>
2022-08-11 22:25:36 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 
 export default class Footer extends Component {
   render() {
-    const appEnv = process.env.REACT_APP_ENV;
     const appVersion = process.env.REACT_APP_VER;
     return (
-        <div className="app-footer card border-0 rounded-0">
-          <small>Environment: { appEnv } | Version: { appVersion }</small>
+        <div className="app-footer">
+          <small>Version: { appVersion }</small>
+          <small>Created by: Shrikant Yadav</small>
         </div>
     )
   }
